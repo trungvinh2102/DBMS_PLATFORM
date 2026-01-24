fae027d05ca0fe5274699fcbd73b4c899cb0d9de
Trung Vinh <vinhnt@aladintech.vn>
2026-01-24 08:23:20 +0700

    initial commit

---
+++ apps/web/src/utils/trpc.ts
@@ -0,0 +1,32 @@
+import type { AppRouter } from "@BI/api/routers/index";
+
+import { QueryCache, QueryClient } from "@tanstack/react-query";
+import { createTRPCClient, httpBatchLink } from "@trpc/client";
+import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
+import { toast } from "sonner";
+
+export const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error, query) => {
+      toast.error(error.message, {
+        action: {
+          label: "retry",
+          onClick: query.invalidate,
+        },
+      });
+    },
+  }),
+});
+
+const trpcClient = createTRPCClient<AppRouter>({
+  links: [
+    httpBatchLink({
+      url: "/api/trpc",
+    }),
+  ],
+});
+
+export const trpc = createTRPCOptionsProxy<AppRouter>({
+  client: trpcClient,
+  queryClient,
+});
